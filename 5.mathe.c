525d141b67001c44fa00c76e556c0a451c257c5a
Jonni <nyaganyamalima47@gmail.com>
2025-12-14 16:11:45 +0300

    math std libraly

---
+++ 5.mathe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int number = 25;
+    int number2 = 27;
+
+    printf("Cube root of %d is %lf",number2,cbrt(number2));
+
+    printf("Square root of %d is %lf",number,sqrt(number));
+    printf("%lf",pow(5,2));
+
+    return 0;
+}
